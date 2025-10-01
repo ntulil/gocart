8dba99e0bd40560212ddd448f1c71f08bd6dc2e3
Leon <leon.ntuli@gmail.com>
2025-10-01 07:38:48 +0200

    axios on app store imported

--- app/store/page.jsx
+++ app/store/page.jsx
@@ -1,11 +1,11 @@
 'use client'
-import { dummyStoreDashboardData } from "@/assets/assets"
 import Loading from "@/components/Loading"
 import { useAuth } from "@clerk/nextjs"
 import { CircleDollarSignIcon, ShoppingBasketIcon, StarIcon, TagsIcon } from "lucide-react"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
 
 export default function Dashboard() {
 
