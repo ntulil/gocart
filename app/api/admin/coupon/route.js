4a7fd693b9859b00dd0b88836874e5e469ac198c
Leon <leon.ntuli@gmail.com>
2025-10-01 08:06:40 +0200

    auth instead of getAuth api admin coupon

--- app/api/admin/coupon/route.js
+++ app/api/admin/coupon/route.js
@@ -1,4 +1,4 @@
-import { getAuth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import authAdmin from "@/middlewares/authAdmin"
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
@@ -8,7 +8,7 @@ import { inngest } from "@/inngest/client";
 // Add new coupon
 export async function POST(req) {
     try {
-        const { userId } = getAuth(req)
+        const { userId } = auth(req)
         const isAdmin = await authAdmin(userId)
 
         if (!isAdmin) {
